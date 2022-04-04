e5c703f2d37824019583b2b5f696749139493a07
Fran Vazquez <fv30vazquez@gmail.com>
2022-04-04 15:44:31 +0200

    Hand

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ capture = cv2.VideoCapture(0)
 capture.set(3, 1280)
 capture.set(4, 720)
 
-#Loop
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
 while True:
     aux, image = capture.read()
+    hands, image = detector.findHands(image)
     cv2.imshow(image)
     cv2.waitKey(1)
